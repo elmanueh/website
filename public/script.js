a68eed66f33b3d6f481410afd592ee1382827956
elmanueh <bernabe2003@gmail.com>
2024-03-25 22:04:29 +0100

    Creating clashfoclans website

---
+++ public/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('enviar').addEventListener('click', function () {
+    let a = document.getElementById('tag').value;
+    alert(a);
+  });
+});
