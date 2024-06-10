aca9b0e5b7206afb59f865f4cdd3c56a0fcea817
elmanueh <bernabe2003@gmail.com>
2024-06-10 11:18:55 +0200

    Create page 404

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,10 @@
+export function Error404() {
+  return (
+    <>
+      <article className="er">
+        <h1 className="er-title">Error 404</h1>
+        <p className="er-text">Ups, esta página no existe</p>
+      </article>
+    </>
+  )
+}
